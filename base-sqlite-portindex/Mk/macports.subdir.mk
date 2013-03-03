b5c65f3866d4b116e1e00663bf58ea94ec6fcc25
Clemens Lang <cal@macports.org>
2013-03-03 20:49:22 +0000

    macports.subdir.mk: support subdirs with spaces
    git-svn-id: https://svn.macports.org/repository/macports/users/cal@103630 d073be05-634f-4543-b044-5fe20cf6d1d6

--- base-sqlite-portindex/Mk/macports.subdir.mk
+++ base-sqlite-portindex/Mk/macports.subdir.mk
@@ -6,24 +6,24 @@
 
 all::
 	@for subdir in $(SUBDIR); do\
-		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		echo ===\> making $@ in $(DIRPRFX)$$subdir; \
+		( cd "$$subdir" && $(MAKE) DIRPRFX="$(DIRPRFX)$$subdir/" $@) || exit 1; \
 	done
 
 clean distclean::
 	@for subdir in $(SUBDIR); do\
-		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		echo ===\> making $@ in $(DIRPRFX)$$subdir; \
+		( cd "$$subdir" && $(MAKE) DIRPRFX="$(DIRPRFX)$$subdir/" $@) || exit 1; \
 	done
 
 test::
 	@for subdir in $(SUBDIR); do\
-		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		echo ===\> making $@ in $(DIRPRFX)$$subdir; \
+		( cd "$$subdir" && $(MAKE) DIRPRFX="$(DIRPRFX)$$subdir/" $@) || exit 1; \
 	done
 
 install::
 	@for subdir in $(SUBDIR); do\
-		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		echo ===\> making $@ in $(DIRPRFX)$$subdir; \
+		( cd "$$subdir" && $(MAKE) DIRPRFX="$(DIRPRFX)$$subdir/" $@) || exit 1; \
 	done
